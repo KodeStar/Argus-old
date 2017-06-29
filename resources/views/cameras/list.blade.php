a82a232bc0702741c75fbd4dae15c14430ad1fea
KodeStar <kodestar@gmail.com>
2017-06-29 15:57:41 +0100

    camera list

--- resources/views/cameras/list.blade.php
+++ resources/views/cameras/list.blade.php
@@ -1,4 +1,21 @@
 <section class="list-camera">
-    <img class="camera-stream" src="{{ $camera->image }}" />
-    <h3>{{ $camera->name }}</h3>
+    <div class="details">
+        <div class="camera-stream"><img class="" src="{{ $camera->image }}" /></div>
+        <div class="info">
+            <h3>{{ $camera->name }}</h3>
+            <dl>
+                <dt>IP Address</dt>
+                <dd>192.168.10.1</dd>
+                <dt>Status</dt>
+                <dd>Active</dd>
+                <dt>Events</dt>
+                <dd>238</dd>
+            </dl>
+        </div>
+        <div class="actions">
+            <a class="btn btn-large" href="">View</a>
+            <a class="btn btn-large" href="">Someting</a>
+            <a class="btn btn-large" href="">Something else</a>
+        </div>
+    </div>
 </section>
